e06c4fe8f160bfaeb637bfb5516063efa3771213
Dan Taeyoung <dan@dantaeyoung.com>
2020-06-19 17:04:13 -0400

    deployment staging working to github pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const fs = require('fs')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/GSAPPSkillTree/'
+    : '/',
     devServer: {
         https: {
           key: fs.readFileSync('./certs/example.com+5-key.pem'),
