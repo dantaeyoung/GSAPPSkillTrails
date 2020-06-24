be54f3bee944a13171bcf22f56a5cc5160b8faa9
Dan Taeyoung <dan@dantaeyoung.com>
2020-06-24 14:47:26 -0400

    comingsoon

---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="comingsoon">
+    <img class="svglogo" src="@/assets/logo.svg" alt="GSAPP SKILL TRAILS" />
+    <h2>coming soon!</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComingSoon",
+  components: {},
+  computed: {
+  },
+  methods: {
+  }
+};
+</script>
+<style scoped lang="scss">
+  .comingsoon {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100vw;
+    z-index: 10000;
+    display: flex;
+    height: 100vh;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #ddd;
+  }
+
+  img {
+  }
+</style>
+
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 
 import App from './App.vue'
 
-Vue.config.productionTip = false
+//Vue.config.productionTip = false
 
 import VueMobileDetection from "vue-mobile-detection";
 
